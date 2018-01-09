195aa5b8b80d1fcd59061a88cc473e87012bedce
Michael Sloan <m.j.sloan88@gmail.com>
2018-01-09 01:05:50 +0000

    Updated articles.js and notes.js files

--- models/notes.js
+++ models/notes.js
@@ -3,9 +3,7 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var NotesSchema = new Schema({
-    // `title` is of type String
     title: String,
-    // `body` is of type String
     body: String
 });
 
